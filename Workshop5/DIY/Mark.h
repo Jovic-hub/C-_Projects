34b539391614bd1ab2b5906bd61739fdf978b553
Jovic-hub <53789339+Jovic-hub@users.noreply.github.com>
2021-04-27 22:43:50 -0300

    Update Mark.h

--- Workshop5/DIY/Mark.h
+++ Workshop5/DIY/Mark.h
@@ -1,10 +1,4 @@
-/**********************************************************
- * Name: Joao Victor Fernandes dos Santos
- * Student ID:155858194
- * Seneca email: jvfernandes-dos-sant@myseneca.ca
- * Section: OOP244 - NGG
- **********************************************************/
- //I have done all the coding by myself and only copied the code that my professor provided to complete my workshopsand assignments.
+
 #ifndef SDDS_MARK_H_
 #define SDDS_MARK_H_
 #include <iostream>
