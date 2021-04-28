297c40d0ff2524d0e592c6346d97f566b7879094
Jovic-hub <53789339+Jovic-hub@users.noreply.github.com>
2021-04-27 22:14:19 -0300

    Update File.h

--- Workshop1/lab/File.h
+++ Workshop1/lab/File.h
@@ -1,9 +1,4 @@
-/**********************************************************
- * Name: Joao Victor Fernandes dos Santos
- * Student ID:155858194
- * Seneca email: jvfernandes-dos-sant@myseneca.ca
- * Section: OOP244 - NGG
- **********************************************************/
+
 #ifndef FILE_H // replace with relevant names
 #define FILE_H
 #include "ShoppingRec.h"
