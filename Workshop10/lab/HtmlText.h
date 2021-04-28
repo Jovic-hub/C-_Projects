79195d0cd4e0b0716c3a4e36de229fd3c44c6508
Jovic-hub <53789339+Jovic-hub@users.noreply.github.com>
2021-04-27 22:34:51 -0300

    Update HtmlText.h

--- Workshop10/lab/HtmlText.h
+++ Workshop10/lab/HtmlText.h
@@ -1,9 +1,4 @@
-/**********************************************************
- * Name: Joao Victor Fernandes dos Santos
- * Student ID:155858194
- * Seneca email: jvfernandes-dos-sant@myseneca.ca
- * Section: OOP244 - NGG
- **********************************************************/
+
 #ifndef SDDS_HTMLTEXT_H__
 #define SDDS_HTMLTEXT_H__
 #include "Text.h"
