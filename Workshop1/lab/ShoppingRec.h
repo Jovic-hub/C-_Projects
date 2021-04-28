e3e06e5faf27c49e1ae2f9da666aa52d716f1d33
Jovic-hub <53789339+Jovic-hub@users.noreply.github.com>
2021-04-27 22:14:35 -0300

    Update ShoppingRec.h

--- Workshop1/lab/ShoppingRec.h
+++ Workshop1/lab/ShoppingRec.h
@@ -1,9 +1,4 @@
-/**********************************************************
- * Name: Joao Victor Fernandes dos Santos
- * Student ID:155858194
- * Seneca email: jvfernandes-dos-sant@myseneca.ca
- * Section: OOP244 - NGG
- **********************************************************/
+
 #ifndef ShoppingRec_H // replace with relevant names
 #define ShoppingRec_H
 
