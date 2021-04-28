0af529be0e615684c2e9e8a7263ae53900695a20
Jovic-hub <53789339+Jovic-hub@users.noreply.github.com>
2021-04-27 22:37:38 -0300

    Update cstring.h

--- Workshop10/lab/cstring.h
+++ Workshop10/lab/cstring.h
@@ -1,9 +1,4 @@
-/**********************************************************
- * Name: Joao Victor Fernandes dos Santos
- * Student ID:155858194
- * Seneca email: jvfernandes-dos-sant@myseneca.ca
- * Section: OOP244 - NGG
- **********************************************************/
+
 #ifndef CSTRING_H
 #define CSTRING_H
 namespace sdds {
