f0569fc54e4d100a85bc0f42bc4c81da677639d4
Jovic-hub <53789339+Jovic-hub@users.noreply.github.com>
2021-04-27 22:56:32 -0300

    Update searchNlist.h

--- Workshop9/lab/searchNlist.h
+++ Workshop9/lab/searchNlist.h
@@ -1,10 +1,4 @@
-/**********************************************************
- * Name: Joao Victor Fernandes dos Santos
- * Student ID:155858194
- * Seneca email: jvfernandes-dos-sant@myseneca.ca
- * Section: OOP244 - NGG
- **********************************************************/
- //I have done all the coding by myself and only copied the code that my professor provided to complete my workshopsand assignments.
+
 #include <iostream>
 #ifndef SDDS_SEARCH_H_
 #define SDDS_SEARCH_H_
