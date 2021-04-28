c0ff20af3b9d31d29b195f37d86c28fbd3f628e4
Jovic-hub <53789339+Jovic-hub@users.noreply.github.com>
2021-04-27 22:28:46 -0300

    Update salReport.cpp

--- Workshop2/lab/salReport.cpp
+++ Workshop2/lab/salReport.cpp
@@ -6,13 +6,7 @@
 // This file tests the lab section of your workshop
 // Do not modify your code
 /////////////////////////////////////////////
-/**********************************************************
- * Name: Joao Victor Fernandes dos Santos
- * Student ID:155858194
- * Seneca email: jvfernandes-dos-sant@myseneca.ca
- * Section: OOP244 - NGG
- **********************************************************/
- //I have done all the coding by myself and only copied the code that my professor provided to complete my workshopsand assignments.
+
 #include "Employee.h"
 using namespace sdds;
 int main() {
