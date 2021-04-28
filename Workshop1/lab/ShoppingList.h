f5ff113d19f9c3cb2731e3e469fdeb7aafe808c4
Jovic-hub <53789339+Jovic-hub@users.noreply.github.com>
2021-04-27 22:14:27 -0300

    Update ShoppingList.h

--- Workshop1/lab/ShoppingList.h
+++ Workshop1/lab/ShoppingList.h
@@ -1,9 +1,4 @@
-/**********************************************************
- * Name: Joao Victor Fernandes dos Santos
- * Student ID:155858194
- * Seneca email: jvfernandes-dos-sant@myseneca.ca
- * Section: OOP244 - NGG
- **********************************************************/
+
 #ifndef ShoppingList_H // replace with relevant names
 #define ShoppingList_H
 namespace sdds {
