a2670554fdc658bd562359b07f09caf2e7d179eb
Jovic-hub <53789339+Jovic-hub@users.noreply.github.com>
2021-04-27 22:36:29 -0300

    Update Text.h

--- Workshop10/lab/Text.h
+++ Workshop10/lab/Text.h
@@ -1,9 +1,4 @@
-/**********************************************************
- * Name: Joao Victor Fernandes dos Santos
- * Student ID:155858194
- * Seneca email: jvfernandes-dos-sant@myseneca.ca
- * Section: OOP244 - NGG
- **********************************************************/
+
 #ifndef SDDS_TEXT_H__
 #define SDDS_TEXT_H__
 using namespace std;
